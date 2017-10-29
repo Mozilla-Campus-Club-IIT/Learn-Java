019ae37479c3337cb18254ad913467f7e42cc36c
Ya-dola <Ya-dola@users.noreply.github.com>
2017-10-29 11:45:51 +0300

    Method: Pass Arguments By: Ya-dola

--- 4. Methods/Method_Params.java
+++ 4. Methods/Method_Params.java
@@ -2,7 +2,7 @@ public class Method_Params {
 
 	//	Method that accepts one int and one string as parameters
 	public void parameters(int number,String string){
-		
+
 	}
 
 }
---
+++ 4. Methods/Method_Pass_Args.java
@@ -0,0 +1,21 @@
+public class Method_Pass_Args {
+
+	public static void main(String[] args) {
+
+		//		Integers to add
+		int num1 = 2;
+		int num2 = 4;
+
+		//		Calling the method add and returning the sum of the 2 integers
+		add(num1, num2);
+
+	}
+
+	//	Method that adds 2 integers and returns the sum of those integers
+	public static int add(int num1, int num2) {
+
+		return num1 + num2;
+
+	}
+
+}
