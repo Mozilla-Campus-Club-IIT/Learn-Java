9e0eeb640f2480d6718dd7df583244adbfdf9fa7
Your Name <you@example.com>
2017-10-30 20:19:42 +0530

    simple methods

---
+++ 4. Methods/SimpleMethod.java
@@ -0,0 +1,19 @@
+public class SimpleMethod {
+    public static void main(String[] args){
+        method1();
+        method2();
+        method3();
+    }
+
+    public static void method1(){
+        System.out.println("Code executed from method1");
+    }
+
+     public static void method2(){
+        System.out.println("Code executed from method2");
+    }
+
+     public static void method3(){
+        System.out.println("Code executed from method3");
+    }
+}
